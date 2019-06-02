b3fb6a480f2123247eb200a5e7bf406a87325ee7
Lorddoyo <johnkojo7777@gmail.com>
2019-06-02 21:41:54 +0300

    deployment

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,7 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[blogboring]'
 
     # Intializing flask extensions
     bootstrap.init_app(app)
